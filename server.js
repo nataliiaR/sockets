502422b645352cdcdf3affe36e9a6e0f0e78d5f8
nataliiaR <nataliya.ryzha@gmail.com>
2016-05-09 12:04:04 -0400

    added listener to frontend

--- server.js
+++ server.js
@@ -7,8 +7,11 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 //for listening for events
-io.on('connection',function(){
+io.on('connection',function(socket){
 	console.log('user connected via socket.io');
+	socket.emit('message', {
+		text: 'Welcome to the chat app'
+	});
 });
 
 app.use(express.static(__dirname + '/public'));
